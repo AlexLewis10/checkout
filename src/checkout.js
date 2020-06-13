af55d7c67b83c5dd850a29dcd0be68fd9629f609
Alex Lewis <alexlewis374@gmail.com>
2020-06-13 13:01:47 +0100

    Remove additional megic numbers and rename functions for readability

--- src/checkout.js
+++ src/checkout.js
@@ -1,7 +1,11 @@
 const A_PRICE = 50
 const A_OFFER_PRICE = 130
+const A_OFFER_NUM = 3
+// A & B Offer number indicates the number of A or B
+// someone buts before it triggers an offer
 const B_PRICE = 30
 const B_OFFER_PRICE = 45
+const B_OFFER_NUM = 2
 const C_PRICE = 20
 const D_PRICE = 15
 const INCORRECT_INPUT = -1
@@ -15,7 +19,7 @@ Shop.prototype.checkout = function (code) {
   }
   let codeArray = code.split('')
 
-  let total = 0
+  let totalPrice = 0
   let numAs = 0
   let numBs = 0
   for (let i = 0; i < codeArray.length; i++) {
@@ -26,34 +30,33 @@ Shop.prototype.checkout = function (code) {
       numBs ++
     }
     if (code[i] === 'C') {
-      total += C_PRICE
+      totalPrice += C_PRICE
     }
     if (code[i] === 'D') {
-      total += D_PRICE
+      totalPrice += D_PRICE
     }
   }
-  total += this._totalAs(numAs)
-  total += this._totalBs(numBs)
-  return total
+  totalPrice += (this._totalOfAs(numAs) + this._totalOfBs(numBs))
+  return totalPrice
 }
 
-Shop.prototype._totalAs = function (numAs) {
+Shop.prototype._totalOfAs = function (numAs) {
   let totalAs = 0
-  if (numAs % 3 === 0) {
-      totalAs += (A_OFFER_PRICE  * (numAs / 3))
+  if (numAs % A_OFFER_NUM === 0) {
+      totalAs += (A_OFFER_PRICE  * (numAs / A_OFFER_NUM))
     } else {
-      let remainder = (numAs % 3)
-      totalAs += (A_OFFER_PRICE  * ((numAs - remainder) / 3)) + (remainder * A_PRICE)
+      let remainder = (numAs % A_OFFER_NUM)
+      totalAs += (A_OFFER_PRICE  * ((numAs - remainder) / A_OFFER_NUM)) + (remainder * A_PRICE)
     }
   return totalAs
 }
 
-Shop.prototype._totalBs = function (numBs) {
+Shop.prototype._totalOfBs = function (numBs) {
   let totalBs = 0
-  if (numBs % 2 === 0) {
-    totalBs += (B_OFFER_PRICE * (numBs / 2))
+  if (numBs % B_OFFER_NUM === 0) {
+    totalBs += (B_OFFER_PRICE * (numBs / B_OFFER_NUM))
   } else { 
-    totalBs += (B_OFFER_PRICE * ((numBs - 1) / 2)) + B_PRICE
+    totalBs += (B_OFFER_PRICE * ((numBs - 1) / B_OFFER_NUM)) + B_PRICE
   }
   return totalBs
 }
